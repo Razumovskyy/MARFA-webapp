783d86ea2cf9844a8a7d7195addeab2fa3debb04
BiTronicsDev <repository@bitronicslab.com>
2024-12-30 19:28:17 +0300

    fix: lib static

---
+++ interface/src/entities/MoleculeSpectre/lib/static.ts
@@ -0,0 +1,45 @@
+import { moleculeSpectreFormData } from "@/entities/MoleculeSpectre"
+
+export const moleculeOptions: { label: string, value: string }[] = [
+  { label: "H2O", value: "H2O" },
+  { label: "CO2", value: "CO2" },
+]
+
+export const targetValues = [
+  { label: "Absorption cross-section [cm^2/mol] (ACS)", value: "ACS"},
+  { label: "Volume absorption coefficient [km^-1] (VAC) ", value: "VAC"}
+]
+
+export const spectralLinesDatabases = [
+  { label: "HITRAN-2020", value: "HITRAN2020"},
+]
+
+export const atmospheres = [
+  { label: "Venus CO2", value: "VenusCO2.dat"},
+  { label: "Venus H2O", value: "VenusH2O.dat"},
+  { label: "VenusCO2 60km", value: "VenusCO2_60km.dat"}
+]
+
+export const chiFactors = [
+  { label: "None", value: "none"},
+  { label: "tonkov", value: "tonkov"},
+  { label: "pollack", value: "pollack"},
+  { label: "perrin", value: "perrin"}
+]
+export const initialFormValues: moleculeSpectreFormData | any | undefined = {
+  species: null,
+  first_spectral_interval: null,
+  second_spectral_interval: null,
+  spectral_line: null,
+  line_cut_off_condition: null,
+  chi_factor: null,
+  target_value: null,
+  file: null,
+  atmosphere: null
+}
+
+export const resolutionsChart = [
+  { label: "coarse", value: "coarse"},
+  { label: "medium", value: "medium"},
+  { label: "high", value: "high"}
+]
---
+++ interface/src/entities/MoleculeSpectre/lib/validationSchema.ts
@@ -0,0 +1,29 @@
+import * as yup from "yup"
+
+export const moleculeSpectreValidationSchema = yup.object().shape({
+  species: yup.object().required("Choose a value."),
+  first_spectral_interval: yup
+    .number()
+    .required("Enter a value.")
+    .typeError("Enter a number")
+    .test("is-less-than-second", "First interval must be less than second interval", function(value) {
+      return value <= this.parent.second_spectral_interval
+    }),
+  second_spectral_interval: yup
+    .number()
+    .required("Enter a value.")
+    .typeError("Enter a number"),
+  spectral_line: yup.object().required("Choose a value."),
+  line_cut_off_condition: yup.number().required("Enter a value"),
+  chi_factor: yup.object().required("Choose a value"),
+  target_value: yup.object().required("Enter a value."),
+  file: yup.mixed().notRequired(),
+  atmosphere: yup.object().notRequired()
+})
+
+export const chartSpectreValidationSchema = yup.object().shape({
+  v1: yup.number().required("Enter a number").typeError("Enter a number"),
+  v2: yup.number().required("Enter a number").typeError("Enter a number"),
+  resolution: yup.string().required("Хуй по фазе").typeError("Говнецо"),
+  level: yup.number().required("Enter a number").typeError("Enter a number")
+})
