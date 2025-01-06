aed3ccb348e9c4d267c71210ae7cb043d56e60d7
Mikhail Razumovskiy <mrazumovskyy@gmail.com>
2025-01-06 18:30:47 +0400

    chore: urls.py docstring

--- api-server/spectres/urls.py
+++ api-server/spectres/urls.py
@@ -1,4 +1,10 @@
-from django.conf.urls.static import static
+"""
+Authors:
+    Mikhail Razumovskii and Denis Astanin, 2025
+
+Description:
+    This module is a part of the MARFA-webapp project.
+"""
 from django.urls import path
 
 from spectres.views import SpectreView
