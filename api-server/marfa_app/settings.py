20ab999d71ce2c2b63f2ffae6cba2dd6d4d218c5
Mikhail Razumovskiy <mrazumovskyy@gmail.com>
2025-01-06 14:50:08 +0400

    ref: path-handling module replacement
    replace os.path with pathlib for readability

--- api-server/marfa_app/settings.py
+++ api-server/marfa_app/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -34,8 +33,8 @@ CORS_ALLOWED_ORIGINS = [
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-SPECTRES_ROOT = os.path.join(MEDIA_ROOT, 'users')
+MEDIA_ROOT = Path(BASE_DIR) / 'media'
+SPECTRES_ROOT = Path(MEDIA_ROOT) / 'users'
 
 # Application definition
 
