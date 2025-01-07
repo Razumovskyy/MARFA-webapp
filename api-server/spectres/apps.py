7ff2e6ac5eec486146c6b0cd0f64364feda78a25
Mikhail Razumovskiy <mrazumovskyy@gmail.com>
2025-01-07 17:06:25 +0400

    ref: update spectres apps.py
    1. added docstring
    2. removed default_auto_field as it is defined globally
    3. set verbose name

--- api-server/spectres/apps.py
+++ api-server/spectres/apps.py
@@ -1,6 +1,13 @@
+"""
+Authors:
+    Mikhail Razumovskii and Denis Astanin, 2025
+
+Description:
+    This module is a part of the MARFA-webapp project.
+"""
 from django.apps import AppConfig
 
 
 class AtmoSpectresConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'spectres'
+    verbose_name = 'Absorption Spectres'
