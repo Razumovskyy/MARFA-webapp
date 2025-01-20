adb276c52f7427a1f139687ac065f2d2973fe2a8
Mikhail Razumovskiy <mrazumovskyy@gmail.com>
2025-01-20 22:52:08 +0400

    Update settings.py

--- api-server/marfa_app/settings.py
+++ api-server/marfa_app/settings.py
@@ -35,7 +35,6 @@ else:  # production
     CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS')
     CURRENT_HOST = env.str('CURRENT_HOST')
     MEDIA_URL = env.str('MEDIA_URL')
-    MEDIA_ROOT = env.str('MEDIA_ROOT')
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
