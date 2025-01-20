9b297ed3fa54dd9dcb56d30430a396b0150e3dc3
Mikhail Razumovskiy <mrazumovskyy@gmail.com>
2025-01-20 16:24:13 +0400

    fix: gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,13 @@ set -e
 cd core
 fpm build
 
-cd ..
+cd ../api-server
 
 # Run Django migrations
-python api-server/manage.py migrate --noinput
+# python manage.py migrate --noinput
 
 # Start Django server in the background
-exec gunicorn api-server.wsgi:application \
+exec gunicorn marfa_app.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 2 \
     --threads 2 \
