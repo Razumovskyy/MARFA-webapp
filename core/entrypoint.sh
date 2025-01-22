3d07d01d3714f3798502ea57afdd4244dbada4a7
Mikhail Razumovskiy <mrazumovskyy@gmail.com>
2025-01-22 21:55:20 +0400

    ref: add gunicorn options

--- core/entrypoint.sh
+++ core/entrypoint.sh
@@ -10,6 +10,8 @@ python manage.py migrate --noinput
 
 exec gunicorn marfa_app.wsgi:application \
     --bind 0.0.0.0:8001 \
-    --workers 5 \
+    --workers 2 \
+    --max-requests 10 \
+    --max-requests-jitter 10 \
     --access-logfile '-' \
     --error-logfile '-'
